1cedd535e6de6771293886dec2366a5b981efac3
Sátán Ádám <satanadam@outlook.com>
2019-04-26 11:43:10 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use("/api/posts", posts);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(__dirname + "/public"));
-    app.get("/.*/", (req, res) => {
+    app.get(/.*/, (req, res) => {
             res.sendFile(__dirname + "/public/index.html");
         }
     )
